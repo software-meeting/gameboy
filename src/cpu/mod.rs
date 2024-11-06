70723c2b3c41afc864757b3953bab524e7369e49
Lucas <me@lhvy.dev>
2024-11-07 00:54:21 +1100

    Add first CPU instructions
    Co-authored-by: quinny <rfquin@users.noreply.github.com>

---
+++ src/cpu/mod.rs
@@ -0,0 +1,230 @@
+mod instructions;
+mod registers;
+
+use instructions::{Instruction, R16, R8, U3};
+use registers::Registers;
+
+struct Cpu {
+    registers: Registers,
+}
+
+impl Cpu {
+    fn execute(&mut self, i: Instruction, memory: &[u8]) -> u8 {
+        match i {
+            Instruction::ADC_A_r8 { r8 } => {
+                self.add_a(self.registers.get_r8(r8), true);
+                4
+            }
+            Instruction::ADC_A_HL => {
+                let rhs = memory[self.registers.get_hl() as usize];
+                self.add_a(rhs, true);
+                8
+            }
+            Instruction::ADC_A_n8 { n8 } => {
+                self.add_a(n8, true);
+                8
+            }
+            Instruction::ADD_A_r8 { r8 } => {
+                self.add_a(self.registers.get_r8(r8), false);
+                4
+            }
+            Instruction::ADD_A_HL => {
+                let rhs = memory[self.registers.get_hl() as usize];
+                self.add_a(rhs, false);
+                8
+            }
+            Instruction::ADD_A_n8 { n8 } => {
+                self.add_a(n8, false);
+                8
+            }
+            Instruction::ADD_HL_r16 { r16 } => {
+                self.add_hl(self.registers.get_r16(r16));
+                8
+            }
+            Instruction::ADD_HL_SP => {
+                self.add_hl(self.registers.sp);
+                8
+            }
+            Instruction::ADD_SP_e8 { e8 } => {
+                self.add_sp(e8);
+                16
+            }
+            Instruction::AND_A_r8 { r8 } => {
+                self.and(self.registers.get_r8(r8));
+                4
+            }
+            Instruction::AND_A_HL => {
+                let rhs = memory[self.registers.get_hl() as usize];
+                self.and(rhs);
+                8
+            }
+            Instruction::AND_A_n8 { n8 } => {
+                self.and(n8);
+                8
+            }
+            Instruction::BIT_U3_R8 { u3, r8 } => {
+                self.bit(u3, self.registers.get_r8(r8));
+                8
+            }
+            Instruction::BIT_U3_HL { u3 } => {
+                let byte = memory[self.registers.get_hl() as usize];
+                self.bit(u3, byte);
+                12
+            }
+            Instruction::CALL_N16 { n16 } => todo!(),
+            Instruction::CALL_CC_N16 { condition, n16 } => todo!(),
+            Instruction::CCF => {
+                self.registers.f.carry = !self.registers.f.carry;
+                4
+            }
+            Instruction::CP_A_R8 { r8 } => todo!(),
+            Instruction::CP_A_HL => todo!(),
+            Instruction::CPL => todo!(),
+            Instruction::DAA => todo!(),
+            Instruction::DEC_R8 { r8 } => todo!(),
+            Instruction::DEC_HL => todo!(),
+            Instruction::DEC_R16 { r16 } => todo!(),
+            Instruction::DEC_SP => todo!(),
+            Instruction::DI => todo!(),
+            Instruction::EI => todo!(),
+            Instruction::HALT => todo!(),
+            Instruction::INC_R8 { r8 } => todo!(),
+            Instruction::INC_HL => todo!(),
+            Instruction::INC_R16 { r16 } => todo!(),
+            Instruction::INC_SP => todo!(),
+            Instruction::JP_N16 { n16 } => todo!(),
+            Instruction::JP_CC_N16 { condition, n16 } => todo!(),
+            Instruction::JP_HL => todo!(),
+            Instruction::JR_N16 { n16 } => todo!(),
+            Instruction::JR_CC_N16 { condition, n16 } => todo!(),
+            Instruction::LD_R8_R8 { dest, src } => todo!(),
+            Instruction::LD_R8_N8 { dest, n8 } => todo!(),
+            Instruction::LD_R16_R16 { dest, src } => todo!(),
+            Instruction::LD_HL_R8 { r8 } => todo!(),
+            Instruction::LD_R8_HL { r8 } => todo!(),
+            Instruction::LD_R16_A { r16 } => todo!(),
+            Instruction::LD_N16_A { n16 } => todo!(),
+            Instruction::LDH_N16_A { n16 } => todo!(),
+            Instruction::LDH_C_A => todo!(),
+            Instruction::LD_A_R16 { r16 } => todo!(),
+            Instruction::LD_A_N16 { n16 } => todo!(),
+            Instruction::LDH_A_R16 { r16 } => todo!(),
+            Instruction::LDH_A_N16 { n16 } => todo!(),
+            Instruction::LD_A_C => todo!(),
+            Instruction::LD_HLI_A => todo!(),
+            Instruction::LD_HLD_A => todo!(),
+            Instruction::LD_A_HLI => todo!(),
+            Instruction::LD_SP_N16 { n16 } => todo!(),
+            Instruction::LD_N16_SP { n16 } => todo!(),
+            Instruction::LD_HL_SPE8 {} => todo!(),
+            Instruction::LD_HL_SPE => todo!(),
+            Instruction::LD_SP_HL => todo!(),
+            Instruction::NOP => todo!(),
+            Instruction::OR_A_R8 { r8 } => todo!(),
+            Instruction::OR_A_HL => todo!(),
+            Instruction::OR_A_N8 { n8 } => todo!(),
+            Instruction::POP_AF => todo!(),
+            Instruction::POP_R16 { r16 } => todo!(),
+            Instruction::PUSH_AF => todo!(),
+            Instruction::PUSH_R16 { r16 } => todo!(),
+            Instruction::RES_U3_R8 { u3, r8 } => todo!(),
+            Instruction::RES_U3_HL { u3 } => todo!(),
+            Instruction::RET_CC { condition } => todo!(),
+            Instruction::RETI => todo!(),
+            Instruction::RL_R8 { r8 } => todo!(),
+            Instruction::RL_HL => todo!(),
+            Instruction::RLA => todo!(),
+            Instruction::RLC_R8 { r8 } => todo!(),
+            Instruction::RLC_HL => todo!(),
+            Instruction::RLCA => todo!(),
+            Instruction::RR_R8 { r8 } => todo!(),
+            Instruction::RR_HL => todo!(),
+            Instruction::RRA => todo!(),
+            Instruction::RRC_R8 { r8 } => todo!(),
+            Instruction::RRC_HL => todo!(),
+            Instruction::RRCA => todo!(),
+            Instruction::RST { vec } => todo!(),
+            Instruction::SBC_A_R8 { r8 } => todo!(),
+            Instruction::SBC_A_HL => todo!(),
+            Instruction::SBC_A_N8 { n8 } => todo!(),
+            Instruction::SCF => todo!(),
+            Instruction::SET_U3_R8 { u3, r8 } => todo!(),
+            Instruction::SET_U3_HL { u3 } => todo!(),
+            Instruction::SLA_R8 { r8 } => todo!(),
+            Instruction::SLA_HL => todo!(),
+            Instruction::SRA_R8 { r8 } => todo!(),
+            Instruction::SRA_HL => todo!(),
+            Instruction::SRL_R8 { r8 } => todo!(),
+            Instruction::SRL_HL => todo!(),
+            Instruction::STOP => todo!(),
+            Instruction::SUB_A_R8 { r8 } => todo!(),
+            Instruction::SUB_A_HL => todo!(),
+            Instruction::SUB_A_N8 { n8 } => todo!(),
+            Instruction::SWAP_R8 { r8 } => todo!(),
+            Instruction::SWAP_HL => todo!(),
+            Instruction::XOR_A_R8 { r8 } => todo!(),
+            Instruction::XOR_A_HL => todo!(),
+            Instruction::XOR_A_N8 { n8 } => todo!(),
+        }
+    }
+
+    fn add_a(&mut self, rhs: u8, carry: bool) {
+        let (mut sum, mut did_overflow) = self.registers.a.overflowing_add(rhs);
+        if carry {
+            let (carry_sum, carry_overflow) = sum.overflowing_add(1);
+            sum = carry_sum;
+            did_overflow = did_overflow || carry_overflow;
+        }
+
+        self.registers.f.zero = sum == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.half_carry = (self.registers.a & 0xF) + (rhs & 0xF) > 0xF;
+        self.registers.f.carry = did_overflow;
+
+        self.registers.a = sum;
+    }
+
+    fn add_hl(&mut self, rhs: u16) {
+        let (sum, did_overflow) = self.registers.get_hl().overflowing_add(rhs);
+
+        self.registers.f.subtract = false;
+        self.registers.f.half_carry = (self.registers.get_hl() & 0xFFF) + (rhs & 0xFFF) > 0xFFF;
+        self.registers.f.carry = did_overflow;
+
+        self.registers.set_hl(sum);
+    }
+
+    fn add_sp(&mut self, rhs: i8) {
+        let rhs = rhs as i16 as u16;
+        let sum = self.registers.sp.wrapping_add(rhs);
+
+        self.registers.f.half_carry = (self.registers.sp & 0xF) + (rhs & 0xF) > 0xF;
+        self.registers.f.carry = (self.registers.sp & 0xFF) + (rhs & 0xFF) > 0xFF;
+
+        self.registers.sp = sum;
+    }
+
+    fn and(&mut self, rhs: u8) {
+        self.registers.a &= rhs;
+        self.registers.f.zero = self.registers.a == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.half_carry = true;
+        self.registers.f.carry = false;
+    }
+
+    fn bit(&mut self, u3: U3, byte: u8) {
+        let bit = match u3 {
+            U3::Zero => byte & 0b1,
+            U3::One => byte & 0b10,
+            U3::Two => byte & 0b100,
+            U3::Three => byte & 0b1000,
+            U3::Four => byte & 0b10000,
+            U3::Five => byte & 0b100000,
+            U3::Six => byte & 0b1000000,
+            U3::Seven => byte & 0b10000000,
+        };
+        self.registers.f.zero = bit != 0;
+        self.registers.f.subtract = false;
+        self.registers.f.half_carry = true;
+    }
+}
---
+++ src/cpu/registers.rs
@@ -0,0 +1,99 @@
+use super::{R16, R8};
+
+pub(crate) struct Flags {
+    pub(crate) zero: bool,
+    pub(crate) subtract: bool,
+    pub(crate) half_carry: bool,
+    pub(crate) carry: bool,
+}
+
+impl From<u8> for Flags {
+    fn from(value: u8) -> Self {
+        Flags {
+            zero: value & 0b10000000 != 0,
+            subtract: value & 0b01000000 != 0,
+            half_carry: value & 0b00100000 != 0,
+            carry: value & 0b00010000 != 0,
+        }
+    }
+}
+
+impl From<&Flags> for u8 {
+    fn from(value: &Flags) -> Self {
+        ((if value.zero { 1 } else { 0 }) << 7)
+            | ((if value.subtract { 1 } else { 0 }) << 6)
+            | ((if value.half_carry { 1 } else { 0 }) << 5)
+            | ((if value.carry { 1 } else { 0 }) << 4)
+    }
+}
+
+pub(crate) struct Registers {
+    pub(crate) a: u8,
+    pub(crate) b: u8,
+    pub(crate) c: u8,
+    d: u8,
+    e: u8,
+    pub(crate) f: Flags,
+    h: u8,
+    l: u8,
+    pub(crate) sp: u16,
+    pc: u16,
+}
+
+impl Registers {
+    fn get_af(&self) -> u16 {
+        ((self.a as u16) << 8) & u8::from(&self.f) as u16
+    }
+
+    fn set_af(&mut self, value: u16) {
+        self.a = (value >> 8) as u8;
+        self.f = Flags::from((value & 0xFF) as u8);
+    }
+
+    fn get_bc(&self) -> u16 {
+        ((self.b as u16) << 8) & self.c as u16
+    }
+
+    fn set_bc(&mut self, value: u16) {
+        self.b = (value >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+
+    fn get_de(&self) -> u16 {
+        ((self.d as u16) << 8) & self.e as u16
+    }
+
+    fn set_de(&mut self, value: u16) {
+        self.d = (value >> 8) as u8;
+        self.e = (value & 0xFF) as u8;
+    }
+
+    pub(crate) fn get_hl(&self) -> u16 {
+        ((self.h as u16) << 8) & self.l as u16
+    }
+
+    pub(crate) fn set_hl(&mut self, value: u16) {
+        self.h = (value >> 8) as u8;
+        self.l = (value & 0xFF) as u8;
+    }
+
+    pub(crate) fn get_r8(&self, register: R8) -> u8 {
+        match register {
+            R8::A => self.a,
+            R8::B => self.b,
+            R8::C => self.c,
+            R8::D => self.d,
+            R8::E => self.e,
+            R8::H => self.h,
+            R8::L => self.l,
+        }
+    }
+
+    pub(crate) fn get_r16(&self, register: R16) -> u16 {
+        match register {
+            R16::BC => self.get_bc(),
+            R16::DE => self.get_de(),
+            R16::HL => self.get_hl(),
+        }
+    }
+}
